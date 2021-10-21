d691255ff29fb7475b7935e2317e29965335fb00
SravanthT <83528948+SravanthT@users.noreply.github.com>
2021-10-21 16:48:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import Topbar from "./topbar";
+import Filterbox from "./filterBy";
+import Trending from "./trending";
+import BlogRender from "./renderdata";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Topbar />
+      <Trending />
+      <Filterbox />
+      <BlogRender />
+    </div>
+  );
+}
---
+++ src/trending.js
@@ -0,0 +1,7 @@
+export default function Trending() {
+  return (
+    <div className="cont-trend">
+      <h1 className="trend-head">Trending Posts</h1>
+    </div>
+  );
+}
